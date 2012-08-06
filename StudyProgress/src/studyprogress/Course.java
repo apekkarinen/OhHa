85f3d17aff786d22d8398bee86a7ec4620edee0c
apekkarinen <antti.pekkar@gmail.com>
2012-08-06 12:37:07 +0300

    Lisätty Course-luokan konstruktori ja testaus

--- StudyProgress/src/studyprogress/Course.java
+++ StudyProgress/src/studyprogress/Course.java
@@ -5,6 +5,25 @@ package studyprogress;
  *
  * @author ausr
  */
+
 public class Course {
+    
+    private String name;
+    private float creditpoints;
+    
+    
+    
+    public Course(String name, float creditpoints) {
+        this.name = name;
+        this.creditpoints = creditpoints;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public float getCreditPoints() {
+        return this.creditpoints;
+    }
     
 }
---
+++ StudyProgress/test/studyprogress/CourseTest.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studyprogress;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author ausr
+ */
+public class CourseTest {
+    
+    public CourseTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+     @Test
+    public void constructorCreatesACourse() {
+         Course course = new Course("Ohjelmoinnin harjoitustyö", 4);
+         assertEquals(4, course.getCreditPoints(), 0.001);
+     }
+}
