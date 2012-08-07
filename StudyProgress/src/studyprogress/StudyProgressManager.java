60696a1a8421b7543c10f7f65db6d8d8ca7e37a0
apekkarinen <antti.pekkar@gmail.com>
2012-08-07 13:17:37 +0300

    Lisätty StudyProgressManager-luokan konstruktori

--- StudyProgress/src/studyprogress/StudyProgressManager.java
+++ StudyProgress/src/studyprogress/StudyProgressManager.java
@@ -5,6 +5,16 @@ package studyprogress;
  *
  * @author Antti Pekkarinen
  */
+import java.util.ArrayList;
+
 public class StudyProgressManager {
+    
+    private ArrayList<String> usernamelist;
+    private Student user;
+    
+    public StudyProgressManager() {
+        user = null;
+        usernamelist = null;
+    }
     
 }
