25e37a17c715a71a2528a45ab644df8102044d41
apekkarinen <antti.pekkar@gmail.com>
2012-08-07 14:44:42 +0300

    Lisätty StudyProgressManager.loadUserListFromFile() ja StudyProgressManager.logInUser sekä testaus

--- StudyProgress/src/studyprogress/StudyProgressManager.java
+++ StudyProgress/src/studyprogress/StudyProgressManager.java
@@ -5,16 +5,55 @@ package studyprogress;
  *
  * @author Antti Pekkarinen
  */
-import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 public class StudyProgressManager {
     
     private ArrayList<String> usernamelist;
     private Student user;
-    
+    private String userfilepath;
+    private String userdirectorypath;
+    private File userfile;
+    private File userdirectory;
+    private Scanner filescanner;
+ 
     public StudyProgressManager() {
-        user = null;
-        usernamelist = null;
+        userdirectorypath = "data";
+        userfilepath = "data/users.txt";
+        userfile = new File(userfilepath);
+        userdirectory = new File(userdirectorypath);
+        usernamelist = loadUserListFromFile();      
     }
     
+    private ArrayList<String> loadUserListFromFile() {
+        
+        String username;
+        ArrayList<String> usernames = new ArrayList<String>();
+        
+        if(userfile.exists()) {
+            try {
+                filescanner = new Scanner(userfile, "UTF-8");
+                
+                while(filescanner.hasNextLine()) {
+                    username = filescanner.nextLine();
+                    usernames.add(username);
+                }
+                
+            } catch (Exception e) {
+                System.out.println("Virhe ladattaessa käyttäjälistaa!");
+            }
+        }
+        return usernames;
+    }
+    
+    public boolean logInUser(String username) {
+        if(usernamelist.contains(username)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
 }
