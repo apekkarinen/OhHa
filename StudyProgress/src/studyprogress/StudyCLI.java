18682d9721151ae87f3f2655016e17c6735c02db
apekkarinen <antti.pekkar@gmail.com>
2012-08-11 11:08:10 +0300

    Muokattu cli-konstruktoria

--- StudyProgress/src/studyprogress/StudyCLI.java
+++ StudyProgress/src/studyprogress/StudyCLI.java
@@ -5,13 +5,17 @@ package studyprogress;
  *
  * @author Antti Pekkarinen
  */
+import java.util.*;
+
 public class StudyCLI {
     private StudyProgressManager manager;
     private Student user;
+    private Scanner userinput;
     
     public StudyCLI(StudyProgressManager manager) {
         this.manager = manager;
         this.user = null;
+        this.userinput = new Scanner(System.in, "UTF-8");
     }
     
     
