e699c14cc6aaecd40d1ef6dde5a15cbed420dcaa
apekkarinen <antti.pekkar@gmail.com>
2012-08-11 11:04:06 +0300

    Lisätty cli-konstruktori

--- StudyProgress/src/studyprogress/StudyCLI.java
+++ StudyProgress/src/studyprogress/StudyCLI.java
@@ -6,5 +6,14 @@ package studyprogress;
  * @author Antti Pekkarinen
  */
 public class StudyCLI {
+    private StudyProgressManager manager;
+    private Student user;
+    
+    public StudyCLI(StudyProgressManager manager) {
+        this.manager = manager;
+        this.user = null;
+    }
+    
+    
     
 }
