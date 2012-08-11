8cab80c4ec1c9e3424b029356d00059b4cb33f38
apekkarinen <antti.pekkar@gmail.com>
2012-08-11 11:51:59 +0300

    StudyCLI.showLogin() ja StudyCLI.promptForNewUserCreation() luotu

--- StudyProgress/src/studyprogress/StudyCLI.java
+++ StudyProgress/src/studyprogress/StudyCLI.java
@@ -18,6 +18,51 @@ public class StudyCLI {
         this.userinput = new Scanner(System.in, "UTF-8");
     }
     
+    public void showLogin() {
+        int helpindex = 0;
+        String selection;
+        while(user == null) {
+            System.out.println("Tervetuloa opintojen seurantajärjestelmään!");
+            System.out.println("Kirjaudu sisään/luo uusi käyttäjä/näytä ohje/poistu (käyttäjänimi/h/q)");
+            selection = userinput.nextLine();
+            
+            if(selection.equals("h")) {
+                showHelp(helpindex);
+            }
+            else if (selection.equals("q")) {
+                break;
+            }
+            else {
+                user = manager.logInUser(selection);
+                if(user == null) {
+                    promptForNewUserCreation(selection);
+                }
+            }
+        }
+    }
+    private void promptForNewUserCreation(String username) {
+        String selection;
+        System.out.println("Käyttäjää nimeltä "+username+" ei löydy, haluatko luoda uuden käyttäjän tällä nimellä? (k/e)");
+        selection = userinput.nextLine();
+        
+        if(selection.equals("k")) {
+            System.out.println("Luodaan uusi käyttäjä '"+username+"'.");
+            manager.createNewUser(username);
+            System.out.println("Käyttäjä "+username+" luotu.");
+        }
+        else if (selection.equals("e")) {
+            System.out.println("Valitsit 'e', Käyttäjää ei luotu.");
+        }
+        else {
+            System.out.println("Tuntematon valinta, käyttäjää ei luotu.");
+        }
+        
+    }
+    private void showHelp(int index) {
+        
+    }
+   
+    
     
     
 }
